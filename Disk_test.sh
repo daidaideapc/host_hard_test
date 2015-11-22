8d3f5ff4710a9c21dd80540cba4f57fb13ea8315
anpengcheng <839274949@qq.com>
2015-11-22 19:01:33 +0800

    finished

--- Disk_test.sh
+++ Disk_test.sh
@@ -8,19 +8,19 @@ fi
 
 echo -e "\n\n"
 
-fio -filename=$1 -direct=1 -iodepth 64 -thread -rw='read' -ioengine=libaio -bs=64k -size=10G -runtime=200 -group_reporting -name=mytestr
+fio -filename=$1 -direct=1 -iodepth 64 -thread -rw=write -ioengine=libaio -bs=64k -size=10G -runtime=200 -group_reporting -name=mytestr
 
 echo -e "\n\n"
 
-fio -filename=$1 -direct=1 -iodepth 64 -thread -rw=write -ioengine=libaio -bs=64k -size=10G -runtime=200 -group_reporting -name=mytestw
+fio -filename=$1 -direct=1 -iodepth 64 -thread -rw='read' -ioengine=libaio -bs=64k -size=10G -runtime=200 -group_reporting -name=mytestw
 
 echo -e "\n\n"
 
-fio -filename=$1 -direct=1 -iodepth 8 -thread -rw=randread -ioengine=libaio -bs=4k -size=10G -runtime=200 -group_reporting -name=mytestrr
+fio -filename=$1 -direct=1 -iodepth 8 -thread -rw=randwrite -ioengine=libaio -bs=4k -size=10G -runtime=200 -group_reporting -name=mytestrr
 
 echo -e "\n\n"
 
-fio -filename=$1 -direct=1 -iodepth 8 -thread -rw=randwrite -ioengine=libaio -bs=4k -size=10G -runtime=200 -group_reporting -name=mytestrw
+fio -filename=$1 -direct=1 -iodepth 8 -thread -rw=randread -ioengine=libaio -bs=4k -size=10G -runtime=200 -group_reporting -name=mytestrw
 
 
 echo -e "\n\n"
